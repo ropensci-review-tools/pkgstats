7c7f96e07e7a27ddc9b9ee0435570c187743bea7
mpadge <mark.padgham@email.com>
2021-04-15 13:35:54 +0200

    use gtags data to get references for objects from ctags

--- R/ctags.R
+++ R/ctags.R
@@ -1,25 +1,64 @@
 
-#' use ctags to parse call data
+#' use ctags and gtags to parse call data
 #'
 #' @param path Path to local repository
 #' @export
-ctags_data <- function (path) {
+tags_data <- function (path) {
 
-    tags <- withr::with_dir (path,
-                             get_ctags ())
+    tags_r <- withr::with_dir (path, get_ctags ("R"))
+    tags_src <- withr::with_dir (path, get_ctags ("src"))
 
-    fns <- tags [tags$kind == "function", ]
-    fn_vars <- tags [tags$kind == "functionVar", ]
+    gtags <- withr::with_dir (path, get_gtags ())
+    ctags <- dplyr::arrange (tags_src, file, start)
+    ctags <- dplyr::filter (ctags, kind %in% c ("class", "function", "struct"))
+    gtags$from <- NA_character_
+    for (f in unique (ctags$file))
+        gtags <- gtags_from_one_file (ctags, gtags, f)
+    gtags <- gtags [which (gtags$tag %in% ctags$tag), ]
 
-    call_graph <- fn_var_call_graph (fns, fn_vars, path)
+    langs <- tags_src [, c ("tag", "language")]
+    langs <- langs [which (!duplicated (langs)), ]
+    gtags$language <- langs$language [match (gtags$tag, langs$tag)]
 
-    return (call_graph)
+    fns_r <- tags_r [tags_r$kind == "function", ]
+    fn_vars_r <- tags_r [tags_r$kind == "functionVar", ]
+
+    call_graph_r <- fn_var_call_graph_r (fns_r,
+                                         fn_vars_r,
+                                         path)
+    call_graph_src <- fn_var_call_graph_src (gtags)
+
+    return (rbind (call_graph_r,
+                   call_graph_src))
 }
 
-get_ctags <- function () {
+#' Get tags for one directory within a package
+#' @param d the directory
+#' @noRd
+get_ctags <- function (d = "R") {
+
+    d <- match.arg (d, c ("R", "src", "inst"))
+
+    path_dir <- file.path (getwd (), d)
+    if (!dir.exists (path_dir))
+        stop ("Directory [", path_dir, "] does not exist")
+
+    # ctags fields defines at
+    # https://docs.ctags.io/en/latest/man/ctags.1.html#extension-fields
+
+    if (d == "R") {
+        fields <- "eFKlnNt"
+    } else if (d == "src") {
+        fields <- "eFKlnN"
+    }
 
     f <- tempfile (fileext = ".txt")
-    cmd <- paste0 ("ctags -R --fields=eFKlnNt -f ", f, " ./R")
+    cmd <- paste0 ("ctags -R --fields=",
+                   fields,
+                   " -f ",
+                   f,
+                   " ",
+                   path_dir)
     system (cmd)
 
     # remove header lines:
@@ -38,11 +77,57 @@ get_ctags <- function () {
 
     tags$start <- as.integer (gsub ("^line\\:", "", tags$start))
     tags$end <- as.integer (gsub ("^end\\:", "", tags$end))
+    tags$file <- gsub (paste0 (getwd (), .Platform$file.sep), "", tags$file)
 
     return (tags)
 }
 
-fn_var_call_graph <- function (fns, fn_vars, path) {
+get_gtags <- function () {
+
+    system ("gtags")
+    x <- system ("global -rx  .", intern = TRUE)
+    # these are fixed width, but only have 4 cols, so can just replace the first
+    # 3 lots of space with single tab characters.
+    for (i in 1:3)
+        x <- sub (" +", "\t", x)
+
+    gtags <- readr::read_delim (x,
+                                delim = "\t",
+                                col_names = FALSE,
+                                col_types = readr::cols ())
+    names (gtags) <- c ("tag", "line", "file", "content")
+
+    # rm header files:
+    gtags <- gtags [which (!grepl ("\\.h$", gtags$file)), ]
+
+    return (gtags)
+}
+
+#' Put ctag tag objects onto the gtag references where they are called from
+#' @param f one '/src' file for for which to assign ctag tags
+#' @noRd
+gtags_from_one_file <- function (ctags, gtags, f) {
+
+    ctags_f <- ctags [ctags$file == f, ]
+    # tags are duplicated for things like class constructors. The duplications are
+    # always embedded within the main definition, so simply removing them reduces
+    # line ranges to main definition only.
+    ctags_f <- ctags_f [which (!duplicated (ctags_f$tag)), ]
+    line_nums <- lapply (seq (nrow (ctags_f)), function (i) {
+                             data.frame (n = i,
+                                         l = seq (ctags_f$start [i],
+                                                  ctags_f$end [i]))
+                        })
+    line_nums <- do.call (rbind, line_nums)
+
+    index <- which (gtags$file == f)
+    items <- line_nums$n [match (gtags$line [index], line_nums$l)]
+    gtags$from [index] <- ctags_f$tag [items]
+
+    return (gtags)
+}
+
+fn_var_call_graph_r <- function (fns, fn_vars, path) {
 
     res <- NULL
 
@@ -58,20 +143,31 @@ fn_var_call_graph <- function (fns, fn_vars, path) {
         pd <- utils::getParseData (parse (file = f_full))
         fn_calls <- pd [pd$text %in% fns$tag &
                         pd$token == "SYMBOL_FUNCTION_CALL", ]
-                    index <- match (fn_calls$line1, fns_index [, 2])
-                    fn_calls$fns_index <- fns_index [index, 1]
-                    fn_calls$name <- fns_f$tag [fn_calls$fns_index]
+        index <- match (fn_calls$line1, fns_index [, 2])
+        fn_calls$fns_index <- fns_index [index, 1]
+        fn_calls$name <- fns_f$tag [fn_calls$fns_index]
 
-                    if (nrow (fn_calls) > 0) {
+        if (nrow (fn_calls) > 0) {
 
-                        res <- rbind (res,
-                                      data.frame (file = f,
-                                                  line1 = fn_calls$line1,
-                                                  line2 = fn_calls$line2,
-                                                  from = fn_calls$name,
-                                                  to = fn_calls$text))
-                    }
+            res <- rbind (res,
+                          data.frame (file = f,
+                                      line1 = fn_calls$line1,
+                                      line2 = fn_calls$line2,
+                                      from = fn_calls$name,
+                                      to = fn_calls$text,
+                                      language = "R"))
+        }
     }
 
     return (res)
+}
+
+fn_var_call_graph_src <- function (gtags) {
+
+    data.frame (file = gtags$file,
+                line1 = gtags$line,
+                line2 = gtags$line,
+                from = gtags$from,
+                to = gtags$tag,
+                language = gtags$language)
 }
