0535d095dcd3a5cd9debd33feb06bc3a8503755d
mpadge <mark.padgham@email.com>
2021-04-20 22:37:51 +0200

    start rd_parse.cpp

--- R/pkgstats-package.R
+++ R/pkgstats-package.R
@@ -5,5 +5,6 @@
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
+#' @useDynLib pkgstats, .registration = TRUE
 ## usethis namespace: end
 NULL
