2271a0873fe18f0ec8380df123b2c72522c81d45
mpadge <mark.padgham@email.com>
2021-04-22 09:13:47 +0200

    add rename_files_in_r fn

--- R/extract_tarball.R
+++ R/extract_tarball.R
@@ -29,7 +29,38 @@ extract_tarball <- function (tarball) {
     if (utils::untar (tarball, exdir = tempdir ()) != 0)
         stop ("Unable to extract tarball to 'tempdir'")
 
-    path <- file.path (tempdir (), flist [1])
+    path <- normalizePath (file.path (tempdir (), flist [1]))
+
+    chk <- rename_files_in_r (path)
+    if (!chk)
+        warning ("Files in .R directory unable to be re-named")
 
     return (path)
+}
+
+#' files in './R' directory may have suffixes of .q, .r, or .s 
+#' https://cran.r-project.org/doc/manuals/R-exts.html#Package-subdirectories
+#' But parsers + cloc only recognise .R/.r, so rename any others
+#' Discovered via BDR's
+#' https://cran.r-project.org/web/packages/boot/index.html
+#' @noRd
+rename_files_in_r <- function (path) {
+
+    fr <- normalizePath (list.files (file.path (path, "R"),
+                                     full.names = TRUE))
+
+    index <- which (!grepl ("\\.r$", fr))
+
+    if (length (index) > 0) {
+
+        chk <- NULL
+
+        for (f in fr [index]) {
+
+            chk <- c (chk,
+                      file.rename (f, gsub ("\\.[a-zA-Z]$", ".R", f)))
+        }
+    }
+
+    return (all (chk))
 }
