a865e7ff3d7532a3434ce59cf56ffdb4f77f4b8f
mpadge <mark.padgham@email.com>
2021-06-16 13:19:23 +0200

    add src/utils.h

---
+++ src/utils.h
@@ -0,0 +1,25 @@
+# pragma once
+
+#include <algorithm>
+#include <vector>
+
+// https://stackoverflow.com/questions/1719070/what-is-the-right-approach-when-using-stl-container-for-median-calculation/1719155#1719155
+
+double median (std::vector <int> &v)
+{
+    if (v.size () == 0)
+        return -1.0;
+
+    int n = static_cast <int> (v.size() / 2);
+
+    std::nth_element (v.begin(), v.begin() + n, v.end());
+    double vn = static_cast <double> (v [static_cast <size_t> (n)]);
+
+    if (v.size() % 2 != 1)
+    {
+        std::nth_element (v.begin(), v.begin() + n - 1, v.end());
+        vn = 0.5 * (vn + v [static_cast <size_t> (n) - 1]);
+    }
+
+    return vn;
+}
