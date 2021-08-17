61d131f15581bfa1aecb500e88e744422eba154e
mpadge <mark.padgham@email.com>
2021-08-17 15:22:17 +0200

    plot vert-only networks; closes #12

--- R/plot.R
+++ R/plot.R
@@ -20,38 +20,62 @@ plot_network <- function (s, plot = TRUE, vis_save = NULL) {
               "from the 'pkgstats' function")
     }
 
-    edges <- data.frame (from = s$network$from,
-                         to = s$network$to,
-                         centrality = s$network$centrality_undir,
-                         language = s$network$language)
-    from <- language <- centrality <- NULL # suppress no visible binding msg
-    edges <- dplyr::count (edges, from, to, language, centrality)
-    edges <- edges [which (!is.na (edges$from)), ]
-    nodes <- unique (c (edges$from, edges$to))
-    nodes <- data.frame (id = nodes,
-                         label = nodes,
-                         name = nodes)
-
-    # match languages on to nodes:
-    langs <- rbind (data.frame (n = edges$from,
-                                lang = edges$language),
-                    data.frame (n = edges$to,
-                                lang = edges$language))
-    langs <- langs [which (!duplicated (langs)), ]
-    nodes$group <- langs$lang [match (nodes$id, langs$n)]
-
-    to <- NULL # no visible binding
-    n <- dplyr::count (edges, to)
-    nodes <- dplyr::left_join (nodes, n, by = c ("name" = "to"))
-    nodes$value <- nodes$n
-    nodes$value [is.na (nodes$value)] <- 1
-
-    edges$width <- edges$centrality * 10 / max (edges$centrality)
     pkg_title <- paste0 (s$desc$package, " network")
-    vn <- visNetwork::visNetwork (nodes, edges,
-                                  main = pkg_title)
-    arrows <- list (to = list (enabled = TRUE, scaleFactor = 0.2))
-    vn <- visNetwork::visEdges (vn, arrows = arrows)
+
+    if (nrow (s$network) == 0L && nrow (s$objects) == 0L) {
+
+        return (NULL)
+
+    } else if (nrow (s$network) == 0L) {
+
+        # pkg has no internal fn_calls, so plot network of vertices only (#12)
+        index <- which (s$objects$kind == "function" &
+                        !grepl ("^anonFunc", s$objects$fn_name))
+        nodes <- s$objects [index, ]
+        nodes <- nodes [which (!duplicated (nodes$fn_name)), ]
+        nodes <- data.frame (id = nodes$fn_name,
+                             label = nodes$fn_name,
+                             name = nodes$fn_name,
+                             group = nodes$language,
+                             value  = 1L)
+
+        vn <- visNetwork::visNetwork (nodes, main = pkg_title)
+
+    } else {
+
+        edges <- data.frame (from = s$network$from,
+                             to = s$network$to,
+                             centrality = s$network$centrality_undir,
+                             language = s$network$language)
+        from <- language <- centrality <- NULL # suppress no visible binding msg
+        edges <- dplyr::count (edges, from, to, language, centrality)
+        edges <- edges [which (!is.na (edges$from)), ]
+        nodes <- unique (c (edges$from, edges$to))
+        nodes <- data.frame (id = nodes,
+                             label = nodes,
+                             name = nodes)
+
+        # match languages on to nodes:
+        langs <- rbind (data.frame (n = edges$from,
+                                    lang = edges$language),
+                        data.frame (n = edges$to,
+                                    lang = edges$language))
+        langs <- langs [which (!duplicated (langs)), ]
+        nodes$group <- langs$lang [match (nodes$id, langs$n)]
+
+        to <- NULL # no visible binding
+        n <- dplyr::count (edges, to)
+        nodes <- dplyr::left_join (nodes, n, by = c ("name" = "to"))
+        nodes$value <- nodes$n
+        nodes$value [is.na (nodes$value)] <- 1
+
+        edges$width <- edges$centrality * 10 / max (edges$centrality)
+
+        vn <- visNetwork::visNetwork (nodes, edges, main = pkg_title)
+        arrows <- list (to = list (enabled = TRUE, scaleFactor = 0.2))
+        vn <- visNetwork::visEdges (vn, arrows = arrows)
+    }
+
     vn <- visNetwork::visLegend (vn, main = "Language")
 
     if (plot | !is.null (vis_save)) {
