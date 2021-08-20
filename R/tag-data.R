ae833c00e784ae3cd6a49bab9a4750f633399f8a
mpadge <mark.padgham@email.com>
2021-08-20 10:40:42 +0200

    add controlParseData fn for multi-byte chars

--- R/tag-data.R
+++ R/tag-data.R
@@ -330,11 +330,9 @@ fn_var_call_graph_r <- function (fns, fn_vars, path) {
         fns_index <- do.call (rbind, fns_index)
 
         f_full <- normalizePath (file.path (path, f))
-        # note: keep.source must be TRUE as it is, for example, switched off in
-        # `rmarkdown` environments, which means no parse data are returned by
-        # getParseData.
-        pd <- utils::getParseData (parse (file = f_full,
-                                          keep.source = TRUE))
+
+        pd <- controlParseData (file = f_full)
+
         fn_calls <- pd [pd$text %in% fns$tag &
                         pd$token == "SYMBOL_FUNCTION_CALL", ]
         index <- match (fn_calls$line1, fns_index [, 2])
@@ -354,6 +352,52 @@ fn_var_call_graph_r <- function (fns, fn_vars, path) {
     }
 
     return (res)
+}
+
+#' Error-controlled version of getParseData
+#'
+#' getParseData can fail even with enforced encoding because of multi-byte
+#' characters. This is a hacky work-around to find and simply strip any such
+#' characters, on the assumption that these can't actually be part of any
+#' language expressions.
+#'
+#' @param file name of R source file
+#' @return contents of `getParseData(x)`
+#' @noRd
+controlParseData <- function (file) {
+
+    # use "latin1" encoding to force re-coding of any non-latin characters:
+    x <- readr::read_lines (file,
+                            locale = readr::locale (encoding = "latin1"),
+                            progress = FALSE)
+
+    # note: keep.source must be TRUE as it is, for example, switched off in
+    # `rmarkdown` environments, which means no parse data are returned by
+    # getParseData.
+    out <- tryCatch (parse (text = x, keep.source = TRUE, encoding = "UTF-8"),
+                     error = function (e) e)
+
+    while (methods::is (out, "simpleError")) {
+
+        g <- gregexpr ("\'.*\'", out$message)
+        ptn <- gsub ("\'", "", regmatches (out$message, g) [[1]])
+        if (substring (ptn, 1, 1) == "\\" & substring (ptn, 2, 2) != "\\") {
+            ptn <- paste0 ("\\", ptn)
+        }
+
+        # Then find line to implement substitution:
+        g <- gregexpr ("\\\"", out$message) [[1]]
+        expr <- gsub ("\"", "", substring (out$message, min (g), max (g)))
+        if (grepl ("\\\\", expr))
+            expr <- gsub ("\\", "\\\\", expr, fixed = TRUE)
+        index <- which (grepl (ptn, x) & grepl (expr, x))
+        x [index] <- gsub (ptn, "", x [index])
+
+        out <- tryCatch (parse (text = x, keep.source = TRUE, encoding = "UTF-8"),
+                         error = function (e) e)
+    }
+
+    return (utils::getParseData (out))
 }
 
 fn_var_call_graph_src <- function (gtags) {
