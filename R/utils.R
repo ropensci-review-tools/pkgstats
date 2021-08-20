5f161fa03365243b753a30f78fec7ae038c3489d
mpadge <mark.padgham@email.com>
2021-08-20 10:47:55 +0200

    amend previous to control_parse fn for re-use elsewhere

--- R/utils.R
+++ R/utils.R
@@ -9,3 +9,49 @@ check_path <- function (path) {
         stop (paste0 ("path [", path, "] does not exist. ",
                       "Did you first 'extract_tarball()'?"))
 }
+
+#' Error-controlled version of parse
+#'
+#' `parse` can fail even with enforced encoding because of multi-byte
+#' characters. This is a hacky work-around to find and simply strip any such
+#' characters, on the assumption that these can't actually be part of any
+#' language expressions.
+#'
+#' @param file name of R source file
+#' @return contents of `parse(file)`
+#' @noRd
+control_parse <- function (file) {
+
+    # use "latin1" encoding to force re-coding of any non-latin characters:
+    x <- readr::read_lines (file,
+                            locale = readr::locale (encoding = "latin1"),
+                            progress = FALSE)
+
+    # note: keep.source must be TRUE as it is, for example, switched off in
+    # `rmarkdown` environments, which means no parse data are returned by
+    # getParseData.
+    out <- tryCatch (parse (text = x, keep.source = TRUE, encoding = "UTF-8"),
+                     error = function (e) e)
+
+    while (methods::is (out, "simpleError")) {
+
+        g <- gregexpr ("\'.*\'", out$message)
+        ptn <- gsub ("\'", "", regmatches (out$message, g) [[1]])
+        if (substring (ptn, 1, 1) == "\\" & substring (ptn, 2, 2) != "\\") {
+            ptn <- paste0 ("\\", ptn)
+        }
+
+        # Then find line to implement substitution:
+        g <- gregexpr ("\\\"", out$message) [[1]]
+        expr <- gsub ("\"", "", substring (out$message, min (g), max (g)))
+        if (grepl ("\\\\", expr))
+            expr <- gsub ("\\", "\\\\", expr, fixed = TRUE)
+        index <- which (grepl (ptn, x) & grepl (expr, x))
+        x [index] <- gsub (ptn, "", x [index])
+
+        out <- tryCatch (parse (text = x, keep.source = TRUE, encoding = "UTF-8"),
+                         error = function (e) e)
+    }
+
+    return (out)
+}
