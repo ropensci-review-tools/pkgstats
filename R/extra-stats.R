67b8a7d060eb01d558e9515ad8e3d9bf3e9743d5
mpadge <mark.padgham@email.com>
2021-09-21 10:11:15 +0200

    fix translations; closes #19

--- R/extra-stats.R
+++ R/extra-stats.R
@@ -70,7 +70,8 @@ get_translations <- function (path) {
                     list.files (po_dir, pattern = "\\.po$"))
         # pkgs may generate translations without having any, and then will only
         # have '.pot' files with no usable translation fields.
-        ll <- ifelse (length (ll) == 0, NA_character_, ll)
+        if (length (ll) == 0L)
+            ll <- NA_character_
     }
 
     return (unique (ll))
