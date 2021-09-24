422168cade57ddc0372dab86429d94527d911a2b
mpadge <mark.padgham@email.com>
2021-09-24 12:17:41 +0200

    add test-archive-trawl

---
+++ tests/testthat/test-archive-trawl.R
@@ -0,0 +1,23 @@
+
+source ("../demo-pkg-script.R")
+
+test_that ("archive trawl", {
+
+    pkg <- make_demo_package ()
+
+    path <- decompose_path (pkg) [[1]] [-1]
+    pkg_name <- path [length (path)]
+    path <- do.call (file.path, as.list (path [-length (path)]))
+
+    expect_error (pkgstats_from_archive (path),
+                  "path must contain a 'tarballs' directory")
+
+    path <- file.path (path, "tarballs")
+    dir.create (path, recursive = TRUE)
+    file.copy (pkg, file.path (path, pkg_name))
+
+    out <- pkgstats_from_archive (path)
+
+    expect_s3_class (out, "data.frame")
+    expect_equal (nrow (out), 1L)
+})
