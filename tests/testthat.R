5597d3cb087ee0536d7c9e8c0db465a8b58f97a9
mpadge <mark.padgham@email.com>
2021-10-14 14:36:25 +0200

    only exported fns in testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,7 +1,9 @@
 library(testthat)
 library(pkgstats)
 
-if (!(has_ctags () & has_gtags ())) {
+chk_ctags <- nzchar (dirname (Sys.which ("ctags")))
+chk_gtags <- nzchar (dirname (Sys.which ("gtags")))
+if (!(chk_ctags & chk_gtags)) {
     ctags_install () # only has effect on unix systems
 }
 
