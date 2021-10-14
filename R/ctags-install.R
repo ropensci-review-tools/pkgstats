5c467e0595777f6f2057a0df071dd485bc73d0a4
mpadge <mark.padgham@email.com>
2021-10-14 13:56:31 +0200

    add final ctags-install fn for #23

--- R/ctags-install.R
+++ R/ctags-install.R
@@ -1,100 +1,82 @@
 # Mostly only lightly adapted from
 # https://github.com/stan-dev/cmdstanr/blob/master/R/install.R
 
-#' Return URL of zip file for latest ctags version
-#' @noRd
-latest_ctags_version <- function() {
+clone_ctags <- function (destdir = NULL) {
 
-    gh_url <- "https://api.github.com/repos/"
-    dest_file <- file.path (tempdir (), "universal-ctags-releases.json")
-
-    if (!file.exists (dest_file)) {
-
-        tags_url <- paste0 (gh_url, "universal-ctags/ctags/git/refs/tags")
-
-        release_list_downloaded <- download_with_retries(tags_url, dest_file)
-        if (!release_list_downloaded) {
-            stop("GitHub download of release list failed.", call. = FALSE)
-        }
+    if (!has_git ()) {
+        stop ("Ctags can only be installed with git", call. = FALSE)
     }
 
-    latest_release <- jsonlite::read_json(dest_file) [[1]]
-    version <- latest_release$ref
-
-    paste0 ("https://github.com/universal-ctags/ctags/archive/",
-            version, ".tar.gz")
-}
-
-download_ctags <- function (destfile = NULL) {
-
-    if (is.null (destfile)) {
-        stop ("destfile must be specified", call. = FALSE)
+    if (is.null (destdir)) {
+        stop ("destdir must be specified", call. = FALSE)
     }
-    destfile <- normalizePath (destfile, mustWork = FALSE)
-    if (!dir.exists (dirname (destfile))) {
-        stop ("Directory [", dirname (destfile),
+    destdir <- normalizePath (destdir, mustWork = FALSE)
+    if (!dir.exists (destdir)) {
+        stop ("Directory [", destdir,
               "] does not exist", call. = FALSE)
     }
 
-    u <- latest_ctags_version ()
-    ctags_downloaded <- download_with_retries (u, destfile)
-    if (!ctags_downloaded)
-        stop ("GitHub download of ctags zip archive failed.", call. = FALSE)
+    u <- "https://github.com/universal-ctags/ctags.git"
 
-    dir_ctags <- file.path (dirname (destfile), "ctags")
-    z <- utils::untar (destfile,
-                       exdir = dir_ctags,
-                       extras = "--strip-components 1")
+    withr::with_dir (destdir, {
+        sys::exec_wait ("git", args = c ("clone", u))
+              })
 
-    if (z != 0)
-        stop ("Failed to extract ctags archive", call. = FALSE)
-
-    return (dir_ctags)
+    return (file.path (destdir, "ctags"))
 }
 
-# download with retries and pauses
-download_with_retries <- function(download_url,
-                                  destination_file,
-                                  retries = 5,
-                                  pause_sec = 5,
-                                  quiet = TRUE) {
+has_git <- function () {
 
-    download_rc <- 1
-    while (retries > 0 && download_rc != 0) {
-        try(
-            suppressWarnings(
-                download_rc <- utils::download.file(url = download_url,
-                                                    destfile = destination_file,
-                                                    quiet = quiet)
-                ),
-            silent = TRUE
-        )
-        if (download_rc != 0) {
-            Sys.sleep(pause_sec)
-        }
-        retries <- retries - 1
-    }
-    if (download_rc == 0) {
-        TRUE
-    } else {
-        FALSE
-    }
+    nzchar (dirname (Sys.which ("git")))
 }
 
 #' Make install ctags from extracted .tar.gz in 'ctags_dir', returned from
 #' `download_ctags()`.
 #' @note Requires 'sudo' and will fail if not
+#' @param ctags_dir Directory of 'ctags' repository cloned with
+#' \link{clone_ctags}.
+#' @param bin_dir Prefix to pass to the `autoconf` configure command
+#' defining location to install the binary, with default of `/usr/local`.
 #' @noRd
-ctags_make <- function (ctags_dir) {
+ctags_make <- function (ctags_dir, bin_dir = NULL) {
 
     f <- tempfile (pattern = "ctags-make-", fileext = ".txt")
 
+    confargs <- NULL
+    if (!is.null (bin_dir)) {
+        if (!dir.exists (bin_dir))
+            stop ("bin_dir [", bin_dir, "] does not exist.", call. = FALSE)
+        confargs <- paste0 ("--prefix=", bin_dir)
+    }
+
     withr::with_dir (ctags_dir, {
         sys::exec_wait ("./autogen.sh", std_out = f)
-        sys::exec_wait ("./configure", args = c ("prefix=/usr"),
-                        std_out = f)
+        sys::exec_wait ("./configure", args = confargs, std_out = f)
         sys::exec_wait ("make", std_out = f)
-        sys::exec_wait ("sudo", args = c ("make", "install"),
-                        std_out = f)
+        sys::exec_wait ("sudo", args = c ("make", "install"), std_out = f)
         })
+}
+
+#' Install 'ctags' from a clone of the 'git' repository
+#'
+#' 'ctags' is installed with this package on both Windows and macOS systems; this
+#' is an additional function to install from source on Unix systems. The
+#' installation requires 'sudo' privileges, and will fail otherwise.
+#'
+#' @param bin_dir Prefix to pass to the `autoconf` configure command
+#' defining location to install the binary, with default of `/usr/local`.
+#' @export
+ctags_install <- function (bin_dir = NULL) {
+
+    if (!.Platform$OS.type == "unix")
+        return (NULL)
+
+    if (ctags_test ()) # already installed and okay
+        return (NULL)
+
+    ctags_dir <- clone_ctags (destdir = tempdir ())
+    ctags_make (ctags_dir, bin_dir)
+
+    if (!has_gtags ())
+        gtags_install ()
 }
