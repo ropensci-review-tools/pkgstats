0798e937c9f69e46c13e0abe53095180b492a1e9
mpadge <mark.padgham@email.com>
2021-10-15 13:29:27 +0200

    spaceout ctags-test.R

--- R/ctags-test.R
+++ R/ctags-test.R
@@ -7,24 +7,28 @@
 #' @export
 ctags_test <- function () {
 
-    if (!has_ctags ())
-        stop ("No ctags installation found.", call. = FALSE)
-    if (!has_gtags ())
-        stop ("No GNU global installation found.", call. = FALSE)
+    if (!has_ctags ()) {
+          stop ("No ctags installation found.", call. = FALSE)
+      }
+    if (!has_gtags ()) {
+          stop ("No GNU global installation found.", call. = FALSE)
+      }
 
     f_in <- tempfile (fileext = ".R")
 
-    x <- c ("G <- 1",                           # globalVar
-            "v <- c(1, 2)",                     # vector
-            "l <- list(3, 4)",                  # list
-            "d <- data.frame(n = v)",           # dataframe + nameattr
-            "f <- function(a) {",               # function
-            "    g <- function (b) a + b",      # function
-            "    w <- c(1, 2)",                 # vector
-            "    m <- list (3, 4)",             # list
-            "    e <- data.frame(n = w)",       # dataframe + nameattr
-            "    L <- 2",                       # functionVar
-            "}")
+    x <- c (
+        "G <- 1", # globalVar
+        "v <- c(1, 2)", # vector
+        "l <- list(3, 4)", # list
+        "d <- data.frame(n = v)", # dataframe + nameattr
+        "f <- function(a) {", # function
+        "    g <- function (b) a + b", # function
+        "    w <- c(1, 2)", # vector
+        "    m <- list (3, 4)", # list
+        "    e <- data.frame(n = w)", # dataframe + nameattr
+        "    L <- 2", # functionVar
+        "}"
+    )
     writeLines (x, f_in)
 
     f_out <- tempfile (fileext = ".txt")
@@ -36,54 +40,66 @@ ctags_test <- function () {
     x <- x [-which (grepl ("^\\!", x))]
     writeLines (x, con = f_out)
 
-    ctypes <- list (readr::col_character (),
-                    readr::col_character (),
-                    readr::col_character (),
-                    readr::col_character (),
-                    readr::col_character (),
-                    readr::col_character (),
-                    readr::col_character ())
+    ctypes <- list (
+        readr::col_character (),
+        readr::col_character (),
+        readr::col_character (),
+        readr::col_character (),
+        readr::col_character (),
+        readr::col_character (),
+        readr::col_character ()
+    )
     cnames <- c ("tag", "file", "content", "kind", "start", "language", "end")
 
     suppressWarnings (
-                      tags <- readr::read_tsv (f_out,
-                                               col_names = cnames,
-                                               col_types = ctypes,
-                                               col_select = cnames,
-                                               progress = FALSE,
-                                               lazy = FALSE)
-                      )
+        tags <- readr::read_tsv (f_out,
+            col_names = cnames,
+            col_types = ctypes,
+            col_select = cnames,
+            progress = FALSE,
+            lazy = FALSE
+        )
+    )
 
-    expected_kinds <- c ("globalVar",
-                         "list",
-                         "dataframe",
-                         "nameattr",
-                         "function",
-                         "function",
-                         "vector",
-                         "list",
-                         "dataframe",
-                         "nameattr",
-                         "functionVar")
+    expected_kinds <- c (
+        "globalVar",
+        "list",
+        "dataframe",
+        "nameattr",
+        "function",
+        "function",
+        "vector",
+        "list",
+        "dataframe",
+        "nameattr",
+        "functionVar"
+    )
 
     ctags_check <- nrow (tags) == length (expected_kinds)
-    if (ctags_check)
-        ctags_check <- all (tags$kind == expected_kinds)
+    if (ctags_check) {
+          ctags_check <- all (tags$kind == expected_kinds)
+      }
 
-    gtags_test <- withr::with_dir (tempdir (),
-                                   system2 ("gtags",
-                                            args = list ('--gtagslabel="new ctags"'), # nolint
-                           stdout = TRUE, stderr = TRUE))
+    gtags_test <- withr::with_dir (
+        tempdir (),
+        system2 ("gtags",
+            args = list ('--gtagslabel="new ctags"'), # nolint
+            stdout = TRUE, stderr = TRUE
+        )
+    )
     gtags_check <- length (gtags_test) == 0L
-    if (!gtags_check)
-        gtags_check <- any (grepl ("error", gtags_check, ignore.case = TRUE))
+    if (!gtags_check) {
+          gtags_check <- any (grepl ("error", gtags_check, ignore.case = TRUE))
+      }
 
     check <- ctags_check & gtags_check
 
     if (!check) {
-        message ("ctags does not function as required; you may need to upgrade? see\n",                 # nolint
-                 "https://github.com/universal-ctags/ctags/blob/master/man/ctags-lang-r.7.rst.in\n",    # nolint
-                 "for expected output")
+        message (
+            "ctags does not function as required; you may need to upgrade? see\n", # nolint
+            "https://github.com/universal-ctags/ctags/blob/master/man/ctags-lang-r.7.rst.in\n", # nolint
+            "for expected output"
+        )
         ret <- FALSE
     } else {
         message ("ctags installation works as expected")
