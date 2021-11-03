f9956a4824d51c52acf8c174f05922f9f51be59f
mpadge <mark.padgham@email.com>
2021-11-03 11:55:54 +0100

    another decompose_path -> fs::path_split for #27

--- R/tag-data.R
+++ R/tag-data.R
@@ -207,7 +207,7 @@ get_ctags <- function (d = "R", has_tabs) {
     tags$end <- as.integer (gsub ("^end\\:", "", tags$end))
 
     files <- fs::path_split (tags$file)
-    len_path_sub <- length (decompose_path (path_sub) [[1]])
+    len_path_sub <- length (fs::path_split (path_sub) [[1]])
     tags$file <- vapply (
         files, function (i) {
             do.call (file.path, as.list (i [-seq (len_path_sub)]))
