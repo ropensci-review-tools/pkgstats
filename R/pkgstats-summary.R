31b085139b340e87139db0dcc3c410cc5e649a91
mpadge <mark.padgham@email.com>
2021-11-09 13:08:25 +0100

    describe return obj of pkgstats_summary; closes #26

--- R/pkgstats-summary.R
+++ R/pkgstats-summary.R
@@ -5,6 +5,26 @@
 #' \link{pkgstats_from_archive}).
 #' @return Summarised version of `s`, as a single row of a standardised
 #' `data.frame` object
+#'
+#' @note Variable names in the summary object use the following abbreviations:
+#' \itemize{
+#'   \item "loc" = Lines-of-Code
+#'   \item "fn" = Function
+#'   \item "n_fns" = Number of functions
+#'   \item "npars" = Number of parameters
+#'   \item "doclines" = Number of documentation lines
+#'   \item "nedges" = Number of edges in function call network, as a count of
+#'   \emph{unique} edges, which may be less than the size of the `network`
+#'   object returned by \link{pkgstats}, because that may include multiple calls
+#'   between identical function pairs.
+#'   \item "n_clusters" = Number of connected clusters within the function call
+#'   network.
+#'   \item "centrality" used as a prefix for several statistics, along with
+#'   "dir" or "undir" for centrality calculated on networks respectively
+#'   constructed with directed or undirected edges; "mn" or "md" for respective
+#'   measures of mean or median centrality, and "no0" for measures excluding
+#'   edges with zero centrality.
+#' }
 #' @family stats
 #' @export
 pkgstats_summary <- function (s = NULL) {
