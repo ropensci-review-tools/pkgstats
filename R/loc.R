ccfa6fe80c2b0e21d8ff2164bf300111946c7fe4
mpadge <mark.padgham@email.com>
2021-11-16 17:54:44 +0100

    use internal tarball in loc_stats example for #30

--- R/loc.R
+++ R/loc.R
@@ -71,18 +71,9 @@ get_file_types <- function (flist) {
 #' @note NA values are returned for directories which do not exist.
 #' @family stats
 #' @examples
-#' \dontrun{
-#' tarball <- "magrittr_2.0.1.tar.gz"
-#' u <- paste0 (
-#'     "https://cran.r-project.org/src/contrib/",
-#'     tarball
-#' )
-#' f <- file.path (tempdir (), tarball)
-#' download.file (u, f)
-#' # have to extract tarball to call function on source code:
+#' f <- system.file ("extdata", "pkgstats_9.9.tar.gz", package = "pkgstats")
 #' path <- extract_tarball (f)
 #' loc_stats (path)
-#' }
 #' @export
 loc_stats <- function (path) {
 
