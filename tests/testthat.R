5671e8a65b3792f9a683eb3fdfc1f7297687aa6a
mpadge <mark.padgham@email.com>
2021-11-16 10:53:30 +0100

    full linux ctags install in configure file

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,10 +1,4 @@
 library (testthat)
 library (pkgstats)
 
-chk_ctags <- nzchar (dirname (Sys.which ("ctags")))
-chk_gtags <- nzchar (dirname (Sys.which ("gtags")))
-if (!(chk_ctags & chk_gtags)) {
-    ctags_install () # only has effect on unix systems
-}
-
 test_check ("pkgstats")
