a74c2b2082d50322c685a0542fd29bcf66705595
mpadge <mark.padgham@email.com>
2021-11-16 17:54:08 +0100

    use internal tarball in extract_tarball example

--- R/extract_tarball.R
+++ R/extract_tarball.R
@@ -6,16 +6,8 @@
 #' @family misc
 #' @export
 #' @examples
-#' \dontrun{
-#' tarball <- "magrittr_2.0.1.tar.gz"
-#' u <- paste0 (
-#'     "https://cran.r-project.org/src/contrib/",
-#'     tarball
-#' )
-#' f <- file.path (tempdir (), tarball)
-#' download.file (u, f)
+#' f <- system.file ("extdata", "pkgstats_9.9.tar.gz", package = "pkgstats")
 #' path <- extract_tarball (f)
-#' }
 extract_tarball <- function (tarball) {
 
     if (!file.exists (tarball)) {
