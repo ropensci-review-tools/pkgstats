b6a1021b63730a87a3472785580553ebf59d40f4
mpadge <mark.padgham@email.com>
2021-11-16 17:53:16 +0100

    use internal tarball in desc-stats example

--- R/desc-stats.R
+++ R/desc-stats.R
@@ -17,18 +17,9 @@ aut_types <- function () {
 #' @inheritParams loc_stats
 #' @family stats
 #' @examples
-#' \dontrun{
-#' tarball <- "magrittr_2.0.1.tar.gz"
-#' u <- paste0 (
-#'     "https://cran.r-project.org/src/contrib/",
-#'     tarball
-#' )
-#' f <- file.path (tempdir (), tarball)
-#' download.file (u, f)
-#' # have to extract tarball to call function on source code:
+#' f <- system.file ("extdata", "pkgstats_9.9.tar.gz", package = "pkgstats")
 #' path <- extract_tarball (f)
 #' desc_stats (path)
-#' }
 #' @export
 desc_stats <- function (path) {
 
