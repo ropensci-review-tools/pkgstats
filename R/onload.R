8afb0721abd8826ed00b26f5a7653323bf592f46
mpadge <mark.padgham@email.com>
2022-01-03 17:48:13 +0100

    unlink gtags-test dir onUnload for #35

--- R/onload.R
+++ R/onload.R
@@ -4,3 +4,11 @@
     bin <- normalizePath (file.path (libname, pkgname, "bin"), mustWork = FALSE)
     Sys.setenv (PATH = paste (bin, Sys.getenv ("PATH"), sep = .Platform$path.sep))
 }
+
+.onUnload <- function (libname, pkgname) { # nolint
+
+    td <- file.path (tempdir (), "pkgstats-gtags-test")
+    if (dir.exists (td)) {
+        chk <- unlist (td, recursive = TRUE)
+    }
+}
