2acd8cf4357dfe919fc77fbff6c4103296e3df86
mpadge <mark.padgham@email.com>
2024-07-10 14:47:41 +0200

    unlink temp 'pkgstats' path in ctags_test

--- R/ctags-test.R
+++ R/ctags-test.R
@@ -104,6 +104,7 @@ ctags_test <- function (quiet = TRUE) {
         }
         dir.create (td)
         chk <- file.copy (pkgstats_path, td, recursive = TRUE)
+        chk <- unlink (pkgstats_path, recursive = TRUE)
         # unlink (pkgstats_path, recursive = TRUE) # done in unload
         cmd <- "export GTAGS_LABEL=new-ctags; gtags"
         gtags_test <- withr::with_dir (file.path (td, "pkgstats"), system (cmd, intern = TRUE))
