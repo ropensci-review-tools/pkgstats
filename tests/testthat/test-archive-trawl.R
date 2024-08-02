8dd30fbe9243770d0ade072e08644f4447e5064a
mpadge <mark.padgham@email.com>
2024-08-02 15:20:40 +0200

    rewrite test-archive-trawl fro #56

--- tests/testthat/test-archive-trawl.R
+++ tests/testthat/test-archive-trawl.R
@@ -1,13 +1,7 @@
-
 test_all <- (identical (Sys.getenv ("MPADGE_LOCAL"), "true") |
     identical (Sys.getenv ("GITHUB_WORKFLOW"), "test-coverage"))
 
-test_that ("archive trawl", {
-
-    if (!test_all) {
-        Sys.setenv ("PKGSTATS_CRAN_TESTS" = "true")
-    }
-
+setup_test_archive <- function () {
     f <- system.file ("extdata", "pkgstats_9.9.tar.gz", package = "pkgstats")
     tarball <- basename (f)
 
@@ -20,22 +14,39 @@ test_that ("archive trawl", {
         file.copy (f, path)
     }
 
-    expect_error (
-        pkgstats_from_archive (path),
-        "Assertion on 'path' failed"
-    )
-    expect_error (
-        pkgstats_fns_from_archive (path),
-        "path must contain a 'tarballs' directory"
-    )
+    list (tarball = tarball, path = path, archive_path = archive_path)
+}
 
-    tarball_path <- file.path (archive_path, "tarballs")
+setup_test_tarball <- function (archive) {
+    tarball_path <- file.path (archive$archive_path, "tarballs")
     if (!dir.exists (tarball_path)) {
         dir.create (tarball_path, recursive = TRUE)
     }
-    if (!file.exists (file.path (tarball_path, tarball))) {
-        file.copy (path, file.path (tarball_path, tarball))
+    if (!file.exists (file.path (tarball_path, archive$tarball))) {
+        file.copy (archive$path, file.path (tarball_path, archive$tarball))
     }
+
+    return (tarball_path)
+}
+
+test_that ("archive trawl", {
+
+    if (!test_all) {
+        Sys.setenv ("PKGSTATS_CRAN_TESTS" = "true")
+    }
+
+    archive <- setup_test_archive ()
+
+    expect_error (
+        pkgstats_from_archive (archive$path),
+        "Assertion on 'path' failed"
+    )
+    expect_error (
+        pkgstats_fns_from_archive (archive$path),
+        "path must contain a 'tarballs' directory"
+    )
+
+    tarball_path <- setup_test_tarball (archive)
 
     out <- pkgstats_from_archive (tarball_path)
 
@@ -48,7 +59,7 @@ test_that ("archive trawl", {
     expect_equal (ncol (out), 3L)
     expect_identical (names (out), c ("package", "version", "fn_name"))
 
-    unlink (archive_path, recursive = TRUE)
+    unlink (archive$archive_path, recursive = TRUE)
     if (!test_all) {
         Sys.unsetenv ("PKGSTATS_CRAN_TESTS")
     }
