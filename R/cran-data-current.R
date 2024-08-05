8e2e38498521ad2f3311465f57b8d1ac33d60bdb
mpadge <mark.padgham@email.com>
2024-08-05 16:54:34 +0200

    start skeleton of cran-current-from-archive fn for #56

---
+++ R/cran-data-current.R
@@ -0,0 +1,8 @@
+#' Reduce `data.frame` of full CRAN archive data to current packages only.
+#'
+#' @inheritParams pkgstats_from_archive
+#' @family archive
+#' @export
+pkgstats_cran_current_from_full <- function (prev_results) {
+
+}
