30858ca41c25cde0b1c8907d10f48b176cc875b4
mpadge <mark.padgham@email.com>
2024-08-05 17:05:04 +0200

    add 'results_file' param to 'cran_current_from_full' fn for #56

--- R/cran-data-current.R
+++ R/cran-data-current.R
@@ -3,7 +3,7 @@
 #' @inheritParams pkgstats_from_archive
 #' @family archive
 #' @export
-pkgstats_cran_current_from_full <- function (prev_results) {
+pkgstats_cran_current_from_full <- function (prev_results, results_file = NULL) {
 
     prev_tarball <- paste0 (prev_results$package, "_", prev_results$version)
 
@@ -11,5 +11,13 @@ pkgstats_cran_current_from_full <- function (prev_results) {
     cran_tarball <- paste0 (cran_pkgs$Package, "_", cran_pkgs$Version)
 
     index <- which (prev_tarball %in% cran_tarball)
-    prev_results [index, ]
+    res <- prev_results [index, ]
+
+    if (!is.null (res) && !is.null (results_file)) {
+
+        results_file <- archive_results_file_name (results_file)
+        saveRDS (res, results_file)
+    }
+
+    return (res)
 }
