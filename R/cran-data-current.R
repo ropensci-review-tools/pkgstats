84200a211b0edb303f4595c461e3d84f41a5ce0b
mpadge <mark.padgham@email.com>
2024-08-05 17:01:28 +0200

    finish cran_current_from_full fn for #56

--- R/cran-data-current.R
+++ R/cran-data-current.R
@@ -5,4 +5,11 @@
 #' @export
 pkgstats_cran_current_from_full <- function (prev_results) {
 
+    prev_tarball <- paste0 (prev_results$package, "_", prev_results$version)
+
+    cran_pkgs <- tools::CRAN_package_db ()
+    cran_tarball <- paste0 (cran_pkgs$Package, "_", cran_pkgs$Version)
+
+    index <- which (prev_tarball %in% cran_tarball)
+    prev_results [index, ]
 }
