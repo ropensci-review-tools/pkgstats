1baa776502dd91c0927c02c82c6ef62bcb61529c
mpadge <mark.padgham@email.com>
2024-10-24 14:24:33 +0200

    split off 'helper-archive-trawl.R' for tests

--- tests/testthat/test-archive-trawl.R
+++ tests/testthat/test-archive-trawl.R
@@ -1,34 +1,6 @@
 test_all <- (identical (Sys.getenv ("MPADGE_LOCAL"), "true") |
     identical (Sys.getenv ("GITHUB_WORKFLOW"), "test-coverage"))
 
-setup_test_archive <- function () {
-    f <- system.file ("extdata", "pkgstats_9.9.tar.gz", package = "pkgstats")
-    tarball <- basename (f)
-
-    archive_path <- file.path (tempdir (), "archive")
-    if (!dir.exists (archive_path)) {
-        dir.create (archive_path)
-    }
-    path <- file.path (archive_path, tarball)
-    if (!file.exists (path)) {
-        file.copy (f, path)
-    }
-
-    list (tarball = tarball, path = path, archive_path = archive_path)
-}
-
-setup_test_tarball <- function (archive) {
-    tarball_path <- file.path (archive$archive_path, "tarballs")
-    if (!dir.exists (tarball_path)) {
-        dir.create (tarball_path, recursive = TRUE)
-    }
-    if (!file.exists (file.path (tarball_path, archive$tarball))) {
-        file.copy (archive$path, file.path (tarball_path, archive$tarball))
-    }
-
-    return (tarball_path)
-}
-
 test_that ("archive trawl", {
 
     if (!test_all) {
