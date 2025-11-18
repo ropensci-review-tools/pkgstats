cc6ea630eb1f0b904509028820ce0bd6f141e6df
mpadge <mark.padgham@email.com>
2025-11-18 13:58:14 +1100

    dontrun -> examplesIf in 'pkgstats' fn for #55

--- R/pkgstats-function.R
+++ R/pkgstats-function.R
@@ -27,14 +27,11 @@
 #' @examples
 #' # 'path' can be path to a package tarball:
 #' f <- system.file ("extdata", "pkgstats_9.9.tar.gz", package = "pkgstats")
-#' \dontrun{
+#' @examplesIf ctags_test ()
 #' s <- pkgstats (f)
-#' }
 #' # or to a source directory:
 #' path <- extract_tarball (f)
-#' \dontrun{
 #' s <- pkgstats (path)
-#' }
 pkgstats <- function (path = ".") {
 
     path <- check_path (path)
